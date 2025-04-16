3e8fe4ee0597ab2b5b89dba72569edc288acf5aa
z3tz3r0 <kittipan.wang@gmail.com>
2025-04-16 13:17:49 +0700

    Add: smallestPalindrome challenge from P'tod

---
+++ 2025-04-16/problems.md
@@ -0,0 +1,38 @@
+## Question 20250416
+
+---
+
+## Smallest Palindrome
+
+กำหนดให้ input เป็น palindrome string (อ่านจากข้างหน้า และอ่านจากข้างหลัง มีค่าเหมือนกัน เช่น tenet, racecar)
+
+จงหา palindrome ที่มีค่าน้อยที่สุดจาก input นี้หลังจากสลับตำแหน่งตัวอักษรแล้ว (เรียงตามตัวอักษร a-z)
+
+_Note: input เป็นอักษรตัวเล็กเท่านั้น และเป็น palindrome string เสมอ_
+
+เช่น
+
+```
+input: str="a"
+output: "a"
+```
+
+```
+input: str="bb"
+output: "bb"
+```
+
+```
+input: str="tenet"
+output: "etnte"
+```
+
+```
+input: str="racecar"
+output: "acrerca"
+```
+
+```
+input: str="dadkdakadkdad"
+output: "aadddkkkdddaa"
+```
---
+++ 2025-04-16/smallestPalindrome.ts
@@ -0,0 +1,49 @@
+function smallestPalindrome(str: string): string {
+    const n: number = str.length;
+    if (n === 0 || typeof str !== "string") return "Non-sense string";
+    if (n === 1) return str;
+
+    if (str !== str.split("").reverse().join("")) {
+        return "This is not a valid palindrome";
+    }
+
+    const map: Record<string, number> = {};
+
+    for (const char of str) {
+        map[char] = (map[char] || 0) + 1;
+    }
+
+    const firstHalf: string[] = [];
+    let middleChar: string = "";
+
+    const sortedChar = Object.keys(map).sort();
+
+    for (const char of sortedChar) {
+        const count = map[char];
+
+        if (count >= 2) {
+            firstHalf.push(char.repeat(Math.floor(count / 2)));
+        }
+
+        if (count % 2 === 1) {
+            middleChar = char;
+        }
+    }
+
+    const firstHalfStr = firstHalf.join("");
+    const secondHalfStr = firstHalf.reverse().join("");
+
+    return firstHalfStr + middleChar + secondHalfStr;
+}
+
+const empty = "";
+const rat = "rat";
+const tenet = "tenet";
+const racecar = "racecar";
+const dadkdakadkdad = "dadkdakadkdad";
+
+console.log(smallestPalindrome(empty)); // 'Non-sense string'
+console.log(smallestPalindrome(rat)); // 'This is not a valid palindrome'
+console.log(smallestPalindrome(tenet)); // 'etnte'
+console.log(smallestPalindrome(racecar)); // 'acrerca'
+console.log(smallestPalindrome(dadkdakadkdad)); // 'aadddkkkdddaa'
