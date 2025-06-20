4eec83131ca775749632452ba01e73afa051b058
z3tz3r0 <kittipan.wang@gmail.com>
2025-06-20 10:56:23 +0700

    feat(sort): Implement merge sort to sort an array from smallest to largest.

---
+++ 2025-06-17/smallToLarge.js
@@ -0,0 +1,30 @@
+function mergeSort(arr) {
+  const n = arr.length;
+  if (n === 1) {
+    return arr;
+  }
+  const mid = Math.floor(n / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid, n);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArray, rightArray) {
+  const sortedArr = [];
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    if (leftArray[0] < rightArray[0]) {
+      sortedArr.push(leftArray[0]);
+      leftArray.shift();
+    } else {
+      sortedArr.push(rightArray[0]);
+      rightArray.shift();
+    }
+  }
+  return sortedArr.concat(leftArray).concat(rightArray);
+}
+
+const arr = [1, 2, 31, 4, 15, 6, 7, 22, 9, 10];
+console.log("Given array is", arr.join(" "));
+const sorted = mergeSort(arr);
+console.log("\nSorted array is", mergeSort(arr).join(" "));
